06c7368fbc49a5fa5b9042b890e73bf82ec9e876
botahamec <botahamec@outlook.com>
2019-09-17 10:54:33 -0400

    changed style a little and made a universal header

---
+++ load_header.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	document.getElementById("header").innerHTML = "<h1>Botahamec</h1>";
+	document.getElementById("nav")
+	.innerHTML = "<a href=\"projects.html\"><button>Projects</button></a><a href=\"blog.html\"><button>Blog</button></a>"
+});
