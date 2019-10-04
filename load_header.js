968e5d4eb5392b1617c5d02a67fb6e7fad9456e6
Mike White <botahamec@outlook.com>
2019-10-04 19:56:56 -0400

    Moved stuff to make more sense

--- load_header.js
+++ load_header.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 	document.getElementById("header").innerHTML = "<h1>Botahamec</h1>";
 	document.getElementById("nav")
-	.innerHTML = "<a href=\"index.html\"><button>Home</button></a><a href=\"projects.html\"><button>Projects</button></a><a href=\"blog.html\"><button>Blog</button></a>"
+	.innerHTML = "<a href=\"https://botahamec.github.io\"><button>Home</button></a><a href=\"https://botahamec.github.io/projects.html\"><button>Projects</button></a><a href=\"https://botahamec.github.io/blog.html\"><button>Blog</button></a>"
 });
--- projects/load_header.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-	document.getElementById("header").innerHTML = "<h1>Botahamec</h1>";
-	document.getElementById("nav")
-	.innerHTML = "<a href=\"../index.html\"><button>Home</button></a><a href=\"../projects.html\"><button>Projects</button></a><a href=\"../blog.html\"><button>Blog</button></a>"
-});
